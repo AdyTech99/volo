8d60ba2fe62443d248ab464a12c9a3b03d75a29e
AdyTech99 <snackwack99@gmail.com>
2025-01-11 11:08:24 +0530

    install npm sfdijoa

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Navigate to the project directory
 
+npm install react react-markdown react-katex remark-math rehype-katex
+
 # Start the Flask server in the background
 echo "Starting Flask server..."
 python flaskserver.py &
