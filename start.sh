6e49cdff6282f7e2641523e5c32974f0089b4a2d
AdyTech99 <snackwack99@gmail.com>
2025-01-11 11:12:07 +0530

    So you'd better work this time

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Navigate to the project directory
 
-npm install react react-markdown react-katex remark-math rehype-katex
+npm install react react-markdown react-katex remark-math rehype-katex --force
 
 # Start the Flask server in the background
 echo "Starting Flask server..."
